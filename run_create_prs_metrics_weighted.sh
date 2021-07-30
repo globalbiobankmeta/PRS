4b44aca5bbf1f741be67786de84d5aaf94cfe150
Ying Wang <ying@atgu-wangmbprt2021.partners.org>
2021-07-30 13:51:13 -0400

    accuracy calculatioin for PRScs-x

---
+++ run_create_prs_metrics_weighted.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+################################################################################
+# Please fill in the below variables
+################################################################################
+Pop="" #e.g., "EUR"
+PHENO="" #e.g., "ASTHMA"
+prop="" #e.g., 0.1
+covs="" #e.g, "Age,Sex"
+pc_numbers="PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10" ##PC1:PC10 is used by default, please change based on your specific Biobank GWAS analyses.
+K=NULL #e.g., 0.058, if not specified, please keep it as NULL, the script will use the proportion of cases in the target population by default
+phenofile="" 
+popfile=""
+covfile=""
+pcfile=""
+cohort="" # Your specific Biobank name
+ldref="" # LD reference panel used, e.g., 1KG or UKBB
+files="" ##list of scorefile, separated by comma
+out_weights="" ##output file name for weights metrics
+wt="" ##variable for weights. NKr2 or h2l_NKr2
+out_prsfile_wt="" ## output file name for weighted PRS
+out_test_metrics="" ## output file name for final accuracy metrics in test cohort
+
+Rscript --slave create_prs_metrics_weighted.R \
+--pop ${Pop} --pheno ${PHENO} \
+--covs ${covs} \
+--prop ${prop} \
+--K ${K} \
+--pc_numbers ${pc_numbers} \
+--phenofile ${phenofile} \
+--popfile ${popfile}  \
+--covfile $covfile \
+--pcfile ${pcfile} \
+--cohort_name ${cohort} \
+--ldref ${ldref} \
+--files ${files} --wt ${wt} \
+--out_weights ${out_weights} \
+--out_prsfile_wt ${out_prsfile_wt} \
+--out_test_metrics ${out_test_metrics}
